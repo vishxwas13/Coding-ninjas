197dd0bd7e2535fec90dac040f361e380e0b0b66
Vishwas Sharma <78073978+vishxwas13@users.noreply.github.com>
2021-09-20 14:58:11 +0530

    Update fibonacci number.java

--- fibonacci number.java
+++ fibonacci number.java
@@ -10,6 +10,18 @@
 // Sample Output 1 :
 // true
 
+import java.util.Scanner;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println(Solution.checkMember(n));
+	}
+
+}
+
 
 
 public class Solution {
